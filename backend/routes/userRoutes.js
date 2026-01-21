c56ff7a4fbfee28b426d376551c339508652e4fc
Shreya Maurya <shreyamaurya0660@gmail.com>
2026-01-21 17:47:09 +0530

    Fix: Import registerUser and correct npm start script

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,12 +2,11 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const auth = require("../middleware/userAuth");
-const { getProfile, uploadAvatar } = require("../controllers/userController");
+const upload = require("../middleware/upload");
+const { getProfile, uploadAvatar, registerUser } = require("../controllers/userController");
 
 router.get("/profile", auth, getProfile);
 router.post("/upload-avatar", auth, uploadAvatar);
-const upload = require("../middleware/upload");
-
 router.post("/register", upload.single("avatar"), registerUser);
 
 module.exports = router;
