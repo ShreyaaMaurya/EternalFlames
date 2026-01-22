b9ec14c3ca5f5397cd1c8b6e37bfded69df0b67b
Shreya Maurya <shreyamaurya0660@gmail.com>
2026-01-22 17:35:55 +0530

    :))

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,31 @@
+// const express = require("express");
+// const router = express.Router();
+// const User = require("../models/user");
+// const auth = require("../middleware/userAuth");
+// const upload = require("../middleware/upload");
+// const { getProfile, uploadAvatar, registerUser } = require("../controllers/userController");
+
+// router.get("/profile", auth, getProfile);
+// router.post("/upload-avatar", auth, uploadAvatar);
+// router.post("/register", upload.single("avatar"), registerUser);
+
+// module.exports = router;
+
+
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user");
 const auth = require("../middleware/userAuth");
-const upload = require("../middleware/upload");
-const { getProfile, uploadAvatar, registerUser } = require("../controllers/userController");
 
+const {
+  registerUser,
+  loginUser,
+  getProfile,
+  uploadAvatar,
+} = require("../controllers/userController");
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
 router.get("/profile", auth, getProfile);
 router.post("/upload-avatar", auth, uploadAvatar);
-router.post("/register", upload.single("avatar"), registerUser);
 
 module.exports = router;
